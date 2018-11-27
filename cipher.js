7344052255850d855d53295af96af3df8ab8a7e9
majosalazar <mariaj.salazarf@gmail.com>
2018-11-27 13:59:05 -0300

    html básico, encode funcinal, no pasa los test

--- cipher.js
+++ cipher.js
@@ -1,8 +1,28 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset, string) => {
+    //aqui guardara el texto
+    let resultEncode = "";
+    //recorre el texto
+    for (let i = 0; i < string.length; i++) {
+      //nueva variable texto en mayuscula
+      let textM = string.toUpperCase();
+      //convierte la letra a numero ascii y lo guarda
+      let codeAscii = textM.charCodeAt(i);
+      //si aparece un 32 lo reemplace por un espacio y que lo guarde
+      if (codeAscii === 32) {
+        resultEncode += " ";
+      }else {
+        //ejecuta la operacion matematica de desplazamiento y la guarda
+        let textDis = ((codeAscii - 65 + parseInt(offset)) % 26) + 65;
+        //guarda el resultado de cada una y la vuelve a convertir en letra
+        resultEncode += String.fromCharCode(textDis);     
+      }
+    } 
+    //devuelve el resultado
+    return resultEncode;
   },
-  decode: () => {
-    /* Acá va tu código */
+
+  decode: (offset, string) => {
+    
   }
 };
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-/* Acá va tu código */
+let offset, string
+
+btnEncode = () => {
+  string = document.getElementById("message").value.toUpperCase();
+  offset = document.getElementById("key").value;
+  console.log("hola " + string + " key " + offset); 
+  let textEncode = window.cipher.encode(offset, string);
+  document.getElementById("resultText").innerHTML = textEncode.toLowerCase();
+
+}
+
+function btnEncode() {
+  
+}
+
+
+//btnEncode(message, key)
+
+let btnDecode = (mensaje, clave) => {
+  console.log("hola " + decodeMessage + " key " + decodeKey);
+
+}
+
+
+resultText.innerHTML = btnDecode(message, key);
+
+let reload = () => {
+
+}
+
