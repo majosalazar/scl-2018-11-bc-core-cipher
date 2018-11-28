15c7018af6bca0d22874f33456132a1c530d6dd8
majosalazar <mariaj.salazarf@gmail.com>
2018-11-27 22:53:46 -0300

    encode y decode funcionales

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,22 @@
 window.cipher = {
   encode: (offset, string) => {
-    //aqui guardara el texto
     let resultEncode = "";
-    //recorre el texto
     for (let i = 0; i < string.length; i++) {
-      //nueva variable texto en mayuscula
-      let textM = string.toUpperCase();
-      //convierte la letra a numero ascii y lo guarda
-      let codeAscii = textM.charCodeAt(i);
-      //si aparece un 32 lo reemplace por un espacio y que lo guarde
-      if (codeAscii === 32) {
-        resultEncode += " ";
-      }else {
-        //ejecuta la operacion matematica de desplazamiento y la guarda
+      let codeAscii = string.charCodeAt(i)
+      console.log(codeAscii)
+      if (codeAscii >= 65 && codeAscii <= 90) {
         let textEncode = ((codeAscii - 65 + parseInt(offset)) % 26) + 65;
-        //guarda el resultado de cada una y la vuelve a convertir en letra
-        resultEncode += String.fromCharCode(textEncode);     
+        resultEncode += String.fromCharCode(textEncode);
+        console.log("mayus")
+      } else if (codeAscii >= 97 && codeAscii <= 122) {
+        let textEncode = ((codeAscii - 97 + parseInt(offset)) % 26) + 97;
+        resultEncode += String.fromCharCode(textEncode);
+        console.log("minus")
+      } else {
+        let textEncode = codeAscii;
+        resultEncode += String.fromCharCode(textEncode);
       }
-    } 
-    //devuelve el resultado
+    }
     return resultEncode;
   },
 
@@ -27,14 +25,12 @@ window.cipher = {
     for (let i = 0; i < string.length; i++) {
       let codeAscii = string.charCodeAt(i)
       console.log(codeAscii)
-      //if (65 <= codeAscii <= 90) {
       if (codeAscii >= 65 && codeAscii <= 90) {
-        let textDecode = ((codeAscii - 65 + parseInt(offset)) % 26) + 65;
+        let textDecode = ((codeAscii - 90 - parseInt(offset)) % 26) + 90;
         resultDecode += String.fromCharCode(textDecode);
         console.log("mayus")
-      //} if (97 <= codeAscii <= 122) {
       } else if (codeAscii >= 97 && codeAscii <= 122) {
-        let textDecode = ((codeAscii - 97 + parseInt(offset)) % 26) + 97;
+        let textDecode = ((codeAscii - 122 - parseInt(offset)) % 26) + 122;
         resultDecode += String.fromCharCode(textDecode);
         console.log("minus")
       } else {
@@ -43,6 +39,5 @@ window.cipher = {
       }
     }
     return resultDecode;
-    
   }
 };
