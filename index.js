379eb68c3f2cff971b24e85a6d2e76efc4ee24ee
majosalazar <mariaj.salazarf@gmail.com>
2018-11-29 12:35:23 -0300

    cifra y decifra numeros

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-let offset, string
-
-
-document.getElementById("btnEncode").onclick = () => {
-  string = document.getElementById("message").value;
-  offset = document.getElementById("key").value;
-  let textEncode = window.cipher.encode(offset, string);
-  document.getElementById("resultText").innerHTML = textEncode;
+window.onload = () => {
+  let offset, string
+  
+  document.getElementById("btnEncode").onclick = () => {
+    string = document.getElementById("message").value;
+    offset = document.getElementById("key").value;
+    let textEncode = cipher.encode(offset, string);
+    document.getElementById("resultText").innerHTML = textEncode;
+  }
+   
+  document.getElementById("btnDecode").onclick = () => {
+    string = document.getElementById("message").value;
+    offset = document.getElementById("key").value;
+    let textDecode = cipher.decode(offset, string);
+    document.getElementById("resultText").innerHTML = textDecode;
+  }
+  
+  document.getElementById("reload").onclick = () => {
+    location.reload();
+  }
 
 }
- 
-document.getElementById("btnDecode").onclick = () => {
-  string = document.getElementById("message").value;
-  offset = document.getElementById("key").value;
-  let textDecode = window.cipher.decode(offset, string);
-  document.getElementById("resultText").innerHTML = textDecode;
 
-}
-
-document.getElementById("reload").onclick = () => {
-  location.reload();
-}
 
 
