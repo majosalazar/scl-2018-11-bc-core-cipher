0ed0e29673899d5680742fe1126cd3577485c2ee
majosalazar <mariaj.salazarf@gmail.com>
2018-11-29 12:35:23 -0300

    cifra y decifra numeros

--- src/cipher.js
+++ src/cipher.js
@@ -3,15 +3,16 @@ window.cipher = {
     let resultEncode = "";
     for (let i = 0; i < string.length; i++) {
       let codeAscii = string.charCodeAt(i)
-      console.log(codeAscii)
       if (codeAscii >= 65 && codeAscii <= 90) {
         let textEncode = ((codeAscii - 65 + parseInt(offset)) % 26) + 65;
         resultEncode += String.fromCharCode(textEncode);
-        console.log("mayus")
       } else if (codeAscii >= 97 && codeAscii <= 122) {
         let textEncode = ((codeAscii - 97 + parseInt(offset)) % 26) + 97;
         resultEncode += String.fromCharCode(textEncode);
-        console.log("minus")
+        console.log(textEncode);
+      } else if (codeAscii >= 48 && codeAscii <= 57) {
+        let textEncode = ((codeAscii - 48 + parseInt(offset)) % 10) + 48;
+        resultEncode += String.fromCharCode(textEncode);
       } else {
         let textEncode = codeAscii;
         resultEncode += String.fromCharCode(textEncode);
@@ -24,15 +25,15 @@ window.cipher = {
     let resultDecode = "";
     for (let i = 0; i < string.length; i++) {
       let codeAscii = string.charCodeAt(i)
-      console.log(codeAscii)
       if (codeAscii >= 65 && codeAscii <= 90) {
         let textDecode = ((codeAscii - 90 - parseInt(offset)) % 26) + 90;
         resultDecode += String.fromCharCode(textDecode);
-        console.log("mayus")
       } else if (codeAscii >= 97 && codeAscii <= 122) {
         let textDecode = ((codeAscii - 122 - parseInt(offset)) % 26) + 122;
         resultDecode += String.fromCharCode(textDecode);
-        console.log("minus")
+      } else if (codeAscii >= 48 && codeAscii <= 57) {
+        let textDecode = ((codeAscii - 57 - parseInt(offset)) % 10) + 57;
+        resultDecode += String.fromCharCode(textDecode);
       } else {
         let textDecode = codeAscii;
         resultDecode += String.fromCharCode(textDecode);
